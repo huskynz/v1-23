d8b8d08e08ae85afaa8a62a5f0f76bae5109f20b
Peter Gallwas <peter@husky.nz>
2023-06-25 20:58:50 +1200

    jgfufkuyfkiuyhfrjyrgkytj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
-
 import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  integrations: [tailwind(), sitemap()]
 });
